6d32826b721ef197de8ccb0a36165e7084231ac3
Javier Ponce <imr3ivaj@gmail.com>
2016-07-12 20:16:24 -0500

    form encapsulated

--- src/components/auth/Auth.react.js
+++ src/components/auth/Auth.react.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AuthForm from './AuthForm.react';
 import { connect } from 'react-redux';
 
 class Auth extends React.Component {
@@ -7,28 +8,7 @@ class Auth extends React.Component {
     return (
       <section className="row">
         <h5>Welcome <strong>{this.props.userName}</strong></h5>
-        <form className="col s12">
-          <div className="row">
-            <div className="input-field col s6">
-              <i className="material-icons prefix">email</i>
-              <input id="email" type="text" />
-              <label htmlFor="email">your@email.com</label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="input-field col s6">
-              <i className="material-icons prefix">lock</i>
-              <input id="password" type="tel" />
-              <label htmlFor="password">password</label>
-            </div>
-          </div>
-          <div className="row">
-            <button className="btn waves-effect waves-light" type="button">
-              Sign In
-              <i className="material-icons right">send</i>
-            </button>
-          </div>
-        </form>
+        <AuthForm />
       </section>
     );
   }
---
+++ src/components/auth/AuthForm.react.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class AuthForm extends React.Component {
+  render () {
+    return (
+      <form className="col s12">
+        <div className="row">
+          <div className="input-field col s6">
+            <i className="material-icons prefix">email</i>
+            <input id="email" type="text" />
+            <label htmlFor="email">your@email.com</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s6">
+            <i className="material-icons prefix">lock</i>
+            <input id="password" type="tel" />
+            <label htmlFor="password">password</label>
+          </div>
+        </div>
+        <div className="row">
+          <button className="btn waves-effect waves-light" type="button">
+            Sign In
+            <i className="material-icons right">send</i>
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default AuthForm;
